3ea710da0bc17b634c8439922820d14fb6abdc20
Lyudmila Shchepina <lyudashchepina1999@gmail.com>
2023-01-07 18:02:21 +0300

    add new password

--- src/components/NewPassword/NewPassword.tsx
+++ src/components/NewPassword/NewPassword.tsx
@@ -1,5 +1,41 @@
 import React from 'react'
 
+import { Button, FormControl, Grid, TextField } from '@mui/material'
+import { useFormik } from 'formik'
+
+// eslint-disable-next-line import/no-unresolved
+import styleForm from './../../styles/form.module.css'
 export const NewPassword = () => {
-  return <div></div>
+  const formik = useFormik({
+    initialValues: {
+      password: '',
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values))
+      window.location.href = '/friday-project-cards/profile'
+    },
+  })
+
+  return (
+    <Grid container justifyContent={'center'}>
+      <Grid item justifyContent={'center'}>
+        <form onSubmit={formik.handleSubmit} className={styleForm.form}>
+          <FormControl>
+            <h3>Create new password</h3>
+            <TextField
+              type="password"
+              id="standard-basic"
+              label="Password"
+              variant="standard"
+              {...formik.getFieldProps('password')}
+            />
+            <p>Create new password and we will send you further instructions to email</p>
+            <Button type={'submit'} variant={'contained'} color={'primary'}>
+              Create new password
+            </Button>
+          </FormControl>
+        </form>
+      </Grid>
+    </Grid>
+  )
 }
