53325de1511f3a6f86bb34e92b0ccd2d4ba6dca5
Alex <alexkazakqq@gmail.com>
2023-01-12 22:51:06 +0300

    add navlink

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -49,27 +49,59 @@ export function Header() {
 }
 
 const NavBar = () => {
+  const activeStyle = {
+    color: '#004d99',
+  }
+
   return (
     <div>
-      <NavLink to={'friday-project-cards/login'} className={s.link}>
+      <NavLink
+        to={'friday-project-cards/login'}
+        className={s.link}
+        style={({ isActive }) => (isActive ? activeStyle : undefined)}
+      >
         Login
       </NavLink>
-      <NavLink to={'friday-project-cards/profile'} className={s.link}>
+      <NavLink
+        to={'friday-project-cards/profile'}
+        className={s.link}
+        style={({ isActive }) => (isActive ? activeStyle : undefined)}
+      >
         Profile
       </NavLink>
-      <NavLink to={'friday-project-cards/registration'} className={s.link}>
+      <NavLink
+        to={'friday-project-cards/registration'}
+        className={s.link}
+        style={({ isActive }) => (isActive ? activeStyle : undefined)}
+      >
         Registration
       </NavLink>
-      <NavLink to={'friday-project-cards/passRecovery'} className={s.link}>
+      <NavLink
+        to={'friday-project-cards/passRecovery'}
+        className={s.link}
+        style={({ isActive }) => (isActive ? activeStyle : undefined)}
+      >
         Pass recovery
       </NavLink>
-      <NavLink to={'friday-project-cards/checkEmail'} className={s.link}>
+      <NavLink
+        to={'friday-project-cards/checkEmail'}
+        className={s.link}
+        style={({ isActive }) => (isActive ? activeStyle : undefined)}
+      >
         Check email
       </NavLink>
-      <NavLink to={'friday-project-cards/newPassword'} className={s.link}>
+      <NavLink
+        to={'friday-project-cards/newPassword'}
+        className={s.link}
+        style={({ isActive }) => (isActive ? activeStyle : undefined)}
+      >
         New password
       </NavLink>
-      <NavLink to={'friday-project-cards/404'} className={s.link}>
+      <NavLink
+        to={'friday-project-cards/404'}
+        className={s.link}
+        style={({ isActive }) => (isActive ? activeStyle : undefined)}
+      >
         404
       </NavLink>
     </div>
